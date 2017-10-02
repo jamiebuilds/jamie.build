1ea1b2b598b3d7a1b8b58a0f665b9c57c33bf6e0
James Kyle <me@thejameskyle.com>
2017-10-02 20:55:49 +1100

    more

--- block-sites.js
+++ block-sites.js
@@ -15,9 +15,13 @@ for (let i = 0; i < BLOCKED_DOMAINS.length; i++) {
 }
 
 if (matched) {
-  document.body.innerHTML = (
-    '<p>This page is unavailable when linked to from ' + matched + '.</p>' +
-    '<p>Please find a less toxic place to spend your time.</p>' +
-    '<p><a href="https://www.youtube.com/watch?v=sC3uHEhjLFo">Here is a great song for your time</a></p>'
-  );
+  document.body.hidden = true;
+  document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML = (
+      '<p>This page is unavailable when linked to from ' + matched + '.</p>' +
+      '<p>Please find a less toxic place to spend your time.</p>' +
+      '<p><a href="https://www.youtube.com/watch?v=_clM6zNYQLQ">Here is a great song for your time</a></p>'
+    );
+    document.body.hidden = false;
+  });
 }
