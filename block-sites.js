1dc275add08d90b04c3e1474aad58d3da1c2e9bb
Isaac Hirschfeld <Meshiest@users.noreply.github.com>
2019-07-17 13:29:50 -0500

    Link HN/Reddit visitors to Dear JavaScript page
    While I do not agree with the idea of blocking your website from members of these communities, I do believe you make some valid points in your letter. This change should bring more attention to your cause rather than generate more toxicity.

--- block-sites.js
+++ block-sites.js
@@ -20,6 +20,7 @@ if (matched) {
     document.body.innerHTML = (
       '<p>This page is unavailable when linked to from ' + matched + '.</p>' +
       '<p>Please find a less toxic place to spend your time.</p>' +
+      '<p>If you are curious as to why you are seeing this, please read my <a href="https://jamie.build/dear-javascript.html">open letter to the community</a>.</p>'
       '<p>Here\'s a song for your time:</p>' +
       '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/Nrnq4SZ0luc?rel=0&amp;controls=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
     );
